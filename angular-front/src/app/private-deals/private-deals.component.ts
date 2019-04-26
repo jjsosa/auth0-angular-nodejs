57777ac96233cde8f240585fc9f6b135c9921f34
Juan José Sosa Osuna <sosaosuna@gmail.com>
2019-04-26 14:44:04 +0200

    Flow added into .ts files

--- angular-front/src/app/private-deals/private-deals.component.ts
+++ angular-front/src/app/private-deals/private-deals.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Deal } from '../deal';
+// import { AuthService } from '../auth.service';
+import { DealService } from '../deal.service';
 
 @Component({
-  selector: 'app-private-deals',
-  templateUrl: './private-deals.component.html',
-  styleUrls: ['./private-deals.component.scss']
+  selector: 'app-public-deals',
+  // We'll use an external file for both the CSS styles and HTML view
+  templateUrl: 'private-deals.component.html',
+  styleUrls: ['private-deals.component.scss']
 })
-export class PrivateDealsComponent implements OnInit {
+export class PrivateDealsComponent implements OnInit, OnDestroy {
+  dealsSub: Subscription;
+  publicDeals: Deal[];
+  error: any;
 
-  constructor() { }
+  constructor(
+    public dealService: DealService,
+    // public authService: AuthService
+  ) { }
 
+  // When this component is loaded, we'll call the dealService and get our public deals.
   ngOnInit() {
+    this.dealsSub = this.dealService
+      .getPrivateDeals()
+      .subscribe(
+        deals => this.publicDeals = deals,
+        err => this.error = err
+      );
   }
 
+  ngOnDestroy() {
+    this.dealsSub.unsubscribe();
+  }
 }
--- angular-front/src/app/public-deals/public-deals.component.ts
+++ angular-front/src/app/public-deals/public-deals.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Deal } from '../deal';
+// import { AuthService } from '../auth.service';
+import { DealService } from '../deal.service';
 
 @Component({
   selector: 'app-public-deals',
-  templateUrl: './public-deals.component.html',
-  styleUrls: ['./public-deals.component.scss']
+  // We'll use an external file for both the CSS styles and HTML view
+  templateUrl: 'public-deals.component.html',
+  styleUrls: ['public-deals.component.scss']
 })
-export class PublicDealsComponent implements OnInit {
+export class PublicDealsComponent implements OnInit, OnDestroy {
+  dealsSub: Subscription;
+  publicDeals: Deal[];
+  error: any;
 
-  constructor() { }
+  constructor(
+    public dealService: DealService,
+    // public authService: AuthService
+  ) { }
 
+  // When this component is loaded, we'll call the dealService and get our public deals.
   ngOnInit() {
+    this.dealsSub = this.dealService
+      .getPublicDeals()
+      .subscribe(
+        deals => this.publicDeals = deals,
+        err => this.error = err
+      );
   }
 
+  ngOnDestroy() {
+    this.dealsSub.unsubscribe();
+  }
 }
