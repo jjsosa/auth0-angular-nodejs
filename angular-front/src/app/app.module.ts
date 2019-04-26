d39c2e6a5701983593ec6f9e5019015fa3e72d46
Juan José Sosa Osuna <sosaosuna@gmail.com>
2019-04-26 14:12:11 +0200

    Initial commit

---
+++ angular-front/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PublicDealsComponent } from './public-deals/public-deals.component';
+import { PrivateDealsComponent } from './private-deals/private-deals.component';
+import { CallbackComponent } from './callback.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PublicDealsComponent,
+    PrivateDealsComponent,
+    CallbackComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-front/src/app/public-deals/public-deals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-deals',
+  templateUrl: './public-deals.component.html',
+  styleUrls: ['./public-deals.component.scss']
+})
+export class PublicDealsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
