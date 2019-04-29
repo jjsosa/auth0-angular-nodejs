9718afbe060759c130f16669d60e2d584cdd3abc
Juan José Sosa Osuna <sosaosuna@gmail.com>
2019-04-29 10:47:17 +0200

    Modified app component

--- angular-front/src/app/app.component.ts
+++ angular-front/src/app/app.component.ts
@@ -1,34 +1,35 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="container">
-      <nav class="navbar navbar-default">
-        <div class="navbar-header">
-          <a class="navbar-brand" routerLink="/dashboard"></a>
-        </div>
-        <ul class="nav navbar-nav">
-          <li>
-            <a routerLink="/deals" routerLinkActive="active">Deals</a>
-          </li>
-          <li>
-            <a routerLink="/special" routerLinkActive="active">Private Deals</a>
-          </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a>Log In</a>
-          </li>
-          <li>
-            <a>Log Out</a>
-          </li>
-        </ul>
-      </nav>
-      <div class="col-sm-12">
-        <router-outlet></router-outlet>
+    <nav class="navbar navbar-default">
+      <div class="navbar-header">
+        <a class="navbar-brand" routerLink="/"></a>
       </div>
+      <ul class="nav navbar-nav">
+        <li>
+          <a routerLink="/deals" routerLinkActive="active">Deals</a>
+        </li>
+        <li>
+          <a routerLink="/special" *ngIf="authService.isLoggedIn" routerLinkActive="active">Private Deals</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a *ngIf="!authService.isLoggedIn" (click)="authService.login()">Log In</a>
+        </li>
+        <li>
+          <a (click)="authService.logout()" *ngIf="authService.isLoggedIn">Log Out</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="col-sm-12">
+      <router-outlet></router-outlet>
     </div>
+  </div>
   `,
   styles: [
     `.navbar-right { margin-right: 0px !important}`
@@ -37,5 +38,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Daily Deals';
 
-  constructor() {}
+  constructor(public authService: AuthService) {}
 }
